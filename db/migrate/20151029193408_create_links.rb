af869a37affe27359dcf32f5225481007b4c4525
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-10-30 10:06:30 -0400

    added user id to links table and generated schema

--- db/migrate/20151029193408_create_links.rb
+++ db/migrate/20151029193408_create_links.rb
@@ -1,8 +1,9 @@
 class CreateLinks < ActiveRecord::Migration
   def change
-    create_table :links do |t|
+    create_table :links do |t|   # need user ID?
       t.string :title
       t.string :url
+      t.integer :user_id
 
       t.timestamps null: false
     end
