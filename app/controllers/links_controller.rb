a223872e8154972139f76f3c4a00bfd47427a074
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-10-30 14:36:59 -0400

    more porgress links controller

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,2 +1,24 @@
 class LinksController < ApplicationController 
+
+  def new
+    @link = Link.new
+    render :new
+  end
+
+  def create 
+    @link = Link.new(:title params[:title], :url params[:url])
+    redirect_to links_path
+  end
+
+  def show
+    @link = Link.find(params[:id])
+    render :show
+  end
+
+  def index
+    @links = Link.title(params[:title])
+    render :index
+  end
+
+
 end
