d7b2122ed5ea52fea2126e4f1f2eb2731052b22b
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-10-30 14:02:06 -0400

    trying to fix schema after pull request

--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20151029194651) do
   create_table "links", force: :cascade do |t|
     t.string   "title"
     t.string   "url"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
