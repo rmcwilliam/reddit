46e1616e9b92aca74d9666660c1ab6d9c9a84423
Carl Westmoreland <beguira86@gmail.com>
2015-10-30 13:32:24 -0400

    fuck git

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController::Base
+
+def new
+	render:new
+end
+
+def create
+	user = User.find_by!(name: params[:name])
+		if user.password == params[:password]
+			session[:user_id] = user.id
+			redirect links_path
+		else
+			redirect login_path
+		end
+end
+
+def destroy
+	session[:user_id] = nil
+	redirect to login_path
+end
+
+
+
+end
