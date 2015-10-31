46f7cd5f0dfbe0e6b9b87ffc2e805b1338ec2803
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-10-31 15:04:14 -0400

    added some validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
   has_many :links
   has_many :comments
+
+  validates_length_of :password, minimum: 7
+  validates_uniqueness_of :name
+  validates_presence_of :name, :password
 end
