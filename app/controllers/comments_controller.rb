035447072b635c071807db30e25e4f296cee8a86
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-10-31 12:05:03 -0400

    rough finished comments controller and view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController 
 
   def create
-    @link = Link.find(params [:id])
+    @link = Link.find(params[:id])
     @link.comments.create!(content: params[:comment])
     redirect_to comments_path(@link)
   end
