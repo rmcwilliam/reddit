0764ebd6ac6f55e0f85eef8761632f1e9d55577c
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-11-11 16:26:25 -0500

    added test create/save user

--- test/controllers/registrations_controller_test.rb
+++ test/controllers/registrations_controller_test.rb
@@ -1,4 +1,20 @@
 require 'test_helper'
 
 class RegistrationsControllerTest  < ActionController::TestCase
+
+  test "new method creates an empty model" do
+  get :new
+  assert_response :success
+  assert_not_nil assigns(:user)
+  assert_template :new
+ end
+
+ test "should create user" do
+  assert_difference("User.count") do
+    post :create,  {username: "billy", email: "ryan@gmail", password: "password"}
+ end
+  assert_redirected_to links_path
+ end
+
 end
+
