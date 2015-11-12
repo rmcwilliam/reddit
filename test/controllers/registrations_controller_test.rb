4999567a82a5e39c0f35c1430e44f633565872cb
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-11-12 17:27:42 -0500

    removed assert success response

--- test/controllers/registrations_controller_test.rb
+++ test/controllers/registrations_controller_test.rb
@@ -3,15 +3,14 @@ require 'test_helper'
 class RegistrationsControllerTest  < ActionController::TestCase
 
   test "new method creates an empty model" do
-  get :new
-  assert_response :success
-  assert_not_nil assigns(:user)
-  assert_template :new
+   get :new
+    assert_not_nil assigns(:user)
+    assert_template :new
  end
 
  test "should create user" do
   assert_difference("User.count") do
-    post :create,  {username: "billy", email: "ryan@gmail", password: "password"}
+   post :create,  {username: "billy", email: "billy@gmail", password: "password"}
  end
   assert_redirected_to links_path
  end
