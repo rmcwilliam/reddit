7d0787f970a49022994431fc826e01349b59d2b8
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-11-17 21:53:00 -0500

    action mailer configuration mandrillapp

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,14 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Add development smtp settings 
+  config.action_mailer.delivery_method = :smtp 
+  config.action_mailer.delivery_settings = {
+    address: 'smtp.mandrillapp.com'
+    port: 587
+    authentication: 'plain'
+    enable_starttls_auto: true
+  }
+
 end
