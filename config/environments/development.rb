00ac6e27472a16be0ec78248758ee95ec885b56a
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-11-17 21:58:49 -0500

    action mailer configuration mandrillapp for real

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,6 +44,7 @@ Rails.application.configure do
   config.action_mailer.delivery_settings = {
     address: 'smtp.mandrillapp.com'
     port: 587
+    username: 'ryanmcwilliam@gmail.com'
     authentication: 'plain'
     enable_starttls_auto: true
   }
