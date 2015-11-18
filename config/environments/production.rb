ee2c50ed7ebf62c647099424261c311546f020c4
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-11-18 18:28:14 -0500

    trying to get heroku mail to work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp 
   config.action_mailer.smtp_settings = {
     address: ENV["SMTP_ADDRESS"],
-    port: 25,
+    port: 465,
     user_name: ENV["SMTP_USERNAME"],
     password: ENV["SMTP_PASSWORD"],
     authentication: 'plain',
