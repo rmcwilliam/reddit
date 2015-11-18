8867119d852d315006862bfb775f0114f59f103a
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-11-18 14:02:20 -0500

    removed and changed default from

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "no-reply@herokuappinstuff.com"
+  default from: "no-reply@ryanmcwilliam.com"
   layout 'mailer'
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ApplicationMailer
-   default from: "no-reply@herokuappinstuff.com"
 
    def welcome_email(user)
     @user = user 
     mail(to: user.email, subject: "Welcome to Reddit, kinda!")
+  end
 end
