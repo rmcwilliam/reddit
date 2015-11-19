5f9b6a55fec951be5958a8bfcfe39333d3820fe7
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-11-18 21:21:30 -0500

    try different port#

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp 
   config.action_mailer.smtp_settings = {
     address: ENV["SMTP_ADDRESS"],
-    port: 465,
+    port: 25,
     user_name: ENV["SMTP_USERNAME"],
     password: ENV["SMTP_PASSWORD"],
     authentication: 'plain',
