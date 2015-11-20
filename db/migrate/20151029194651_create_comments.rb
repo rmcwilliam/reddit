8deb7b44413bda4dc1b32a241bf81e388c7ed727
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-11-19 21:07:06 -0500

    incorrect implementation

--- db/migrate/20151029194651_create_comments.rb
+++ db/migrate/20151029194651_create_comments.rb
@@ -1,9 +1,9 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
-      t.string :content, null: false
-      t.integer :link_id, null: false
-      t.integer :user_id, null: false
+      t.string :content
+      t.integer :link_id
+      t.integer :user_id
 
       t.timestamps null: false
     end
