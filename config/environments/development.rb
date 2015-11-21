e878589743d697857d80cac26772dd5b3c4fd5a4
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-11-21 18:09:10 -0500

    changed port to work with foreman

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   # Define default url option
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 5000 }
 
   # Add development smtp settings 
   config.action_mailer.delivery_method = :smtp 
