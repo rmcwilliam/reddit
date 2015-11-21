2485d1450891c14fcbe83df42ec76d0fb123d8a3
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-11-21 17:54:25 -0500

    removed unwanted comma

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,7 +43,6 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Add development smtp settings 
-  config.action_mailer.delivery_method = :sendmail
   config.action_mailer.delivery_method = :smtp 
   config.action_mailer.smtp_settings = {
     address: ENV["SMTP_ADDRESS"],
